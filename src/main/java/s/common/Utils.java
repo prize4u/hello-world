451784513b0dd54e9ff49d4a61137ac629247f81
prize4u <prizefrigh@163.com>
2017-02-17 15:43:24 +0800

    delete line from sample back

--- src/main/java/s/common/Utils.java
+++ src/main/java/s/common/Utils.java
@@ -1,1 +1,1 @@
-package s.common;public class Utils {	public static void main(String[] args) {		System.out.println("this is utils class");	}		public void print() {		System.out.println("sample");		System.out.println("new line");	}	}
+package s.common;public class Utils {	public static void main(String[] args) {		System.out.println("this is utils class");	}		public void print() {		System.out.println("sample");	}	}
