918cd2b4aef35fa0e9fa7fec78bc382dd08be937
prize4u <prizefrigh@163.com>
2017-02-17 15:17:33 +0800

    change newMethod form sample

--- src/main/java/s/common/Utils.java
+++ src/main/java/s/common/Utils.java
@@ -1,1 +1,1 @@
-package s.common;public class Utils {	public static void main(String[] args) {		System.out.println("this is utils class");	}		public void print() {		System.out.println("sample");		System.out.println("new line");	}		public void newMethod() {		System.out.println("new method");	}}
+package s.common;public class Utils {	public static void main(String[] args) {		System.out.println("this is utils class");	}		public void print() {		System.out.println("sample");		System.out.println("new line");	}		public void newMethod() {		System.out.println("abcd");	}}
